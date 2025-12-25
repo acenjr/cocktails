b36c58742d68fc5740820b03dddfb24295b4498b
Kelvin Njeru <njrkelvin@gmail.com>
2025-12-25 14:13:33 +0300

    implemented menu section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
-import { navLinks } from '../../constants'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+import React from "react";
+import { navLinks } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Navbar = () => {
-useGSAP(()=>{
+  useGSAP(() => {
     const navTween = gsap.timeline({
-        scrollTrigger: {
-            trigger: 'nav', start: 'bottom top'
-        }
-    })
-    navTween.fromTo('nav', {backgroundColor: 'transparent'}, {backgroundColor: '#00000050', backgroundFilter: 'blur(10px', duration:1, ease: 'power1.inOut' })
-})
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
 
   return (
-   <nav>
-    <div>
-        <a href="#home" className='flex items-center gap-2'>
-            <img src="/images/logo.png" alt="logo" />
-            <p>Golden Elixir</p>
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Golden Elixir</p>
         </a>
 
         <ul>
-            {navLinks.map((link)=>(
-                <li key={link.id}>
-<a href={`#${link.id}`}>{link.title}</a>
-                </li>
-            ))}
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
         </ul>
-    </div>
-   </nav>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
